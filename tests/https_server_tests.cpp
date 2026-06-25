3fa81d9ffbc2cbf865deaa29d0e04daaa662c58e
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2026-06-25 20:16:55 +0200

    Extracts SelfSignedCert helper into a shared test header

--- tests/https_server_tests.cpp
+++ tests/https_server_tests.cpp
@@ -9,26 +9,18 @@
 
 #include <gtest/gtest.h>
 #include "sap_http/net/http.h"
+#include "self_signed_cert.h"
 
 #include <sap_network/tls_socket.h>
 #include <sap_network/tcp_socket.h>
 
-#include <openssl/evp.h>
-#include <openssl/pem.h>
-#include <openssl/x509.h>
-#include <openssl/x509v3.h>
-
 #include <atomic>
 #include <chrono>
-#include <cstdio>
-#include <filesystem>
 #include <future>
-#include <random>
 #include <string>
 #include <thread>
 #include <vector>
 
-namespace fs = std::filesystem;
 using namespace std::chrono_literals;
 
 // ---------------------------------------------------------------------------
@@ -69,124 +61,6 @@ namespace {
     constexpr u16 PORT_RESPONSE_HEADERS      = 12030;
     constexpr u16 PORT_DISCONNECT_DURING_RECV = 12031;
 }
-
-// ---------------------------------------------------------------------------
-// Self-signed cert + key fixture (programmatic, per-test).
-// Mirrors sap_network's SelfSignedCert helper. RAII — temp dir cleaned up in
-// the dtor. CA mode generates a self-signing CA suitable for issuing client
-// certs (used by the mTLS tests).
-// ---------------------------------------------------------------------------
-
-class SelfSignedCert {
-public:
-    enum class EKind { Server, ClientSignedByThis };
-
-    explicit SelfSignedCert(stl::string cn = "localhost", bool is_ca = false) {
-        std::random_device rd;
-        m_dir = fs::temp_directory_path() / fs::path{"sap_http_tls_test_" + std::to_string(rd())};
-        fs::create_directories(m_dir);
-        cert_file = (m_dir / "cert.pem").string();
-        key_file  = (m_dir / "key.pem").string();
-        m_pkey = ::EVP_RSA_gen(2048);
-        m_x509 = make_cert(cn, m_pkey, /*signer_pkey=*/m_pkey, /*signer_x509=*/nullptr, is_ca);
-        write_pair(cert_file, key_file, m_x509, m_pkey);
-    }
-
-    ~SelfSignedCert() {
-        if (m_x509) ::X509_free(m_x509);
-        if (m_pkey) ::EVP_PKEY_free(m_pkey);
-        std::error_code ec;
-        fs::remove_all(m_dir, ec);
-    }
-
-    SelfSignedCert(const SelfSignedCert&) = delete;
-    SelfSignedCert& operator=(const SelfSignedCert&) = delete;
-
-    // Issue a client cert signed by *this* CA. Returns paths to a pair of
-    // files (cert.pem, key.pem) inside this CA's temp dir.
-    struct Issued {
-        stl::string cert_file;
-        stl::string key_file;
-    };
-    Issued issue_client(stl::string cn) {
-        EVP_PKEY* client_pkey = ::EVP_RSA_gen(2048);
-        X509*     client_x509 = make_cert(cn, client_pkey, m_pkey, m_x509, /*is_ca=*/false);
-        Issued i;
-        i.cert_file = (m_dir / (cn + "_cert.pem")).string();
-        i.key_file  = (m_dir / (cn + "_key.pem")).string();
-        write_pair(i.cert_file, i.key_file, client_x509, client_pkey);
-        ::X509_free(client_x509);
-        ::EVP_PKEY_free(client_pkey);
-        return i;
-    }
-
-    stl::string cert_file;
-    stl::string key_file;
-
-private:
-    static X509* make_cert(const stl::string& cn, EVP_PKEY* pkey,
-                           EVP_PKEY* signer_pkey, X509* signer_x509, bool is_ca) {
-        X509* x = ::X509_new();
-        ::ASN1_INTEGER_set(::X509_get_serialNumber(x), std::rand());
-        ::X509_gmtime_adj(::X509_getm_notBefore(x), 0);
-        ::X509_gmtime_adj(::X509_getm_notAfter(x), 31536000L); // 1 year
-        ::X509_set_pubkey(x, pkey);
-        X509_NAME* name = ::X509_get_subject_name(x);
-        ::X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC,
-            reinterpret_cast<const unsigned char*>(cn.c_str()), -1, -1, 0);
-        if (signer_x509)
-            ::X509_set_issuer_name(x, ::X509_get_subject_name(signer_x509));
-        else
-            ::X509_set_issuer_name(x, name);
-
-        X509V3_CTX v3ctx;
-        X509V3_set_ctx_nodb(&v3ctx);
-        ::X509V3_set_ctx(&v3ctx, signer_x509 ? signer_x509 : x, x, nullptr, nullptr, 0);
-        if (is_ca) {
-            if (X509_EXTENSION* ext = ::X509V3_EXT_conf_nid(nullptr, &v3ctx,
-                    NID_basic_constraints, "critical,CA:TRUE")) {
-                ::X509_add_ext(x, ext, -1);
-                ::X509_EXTENSION_free(ext);
-            }
-            if (X509_EXTENSION* ext = ::X509V3_EXT_conf_nid(nullptr, &v3ctx,
-                    NID_key_usage, "critical,keyCertSign,cRLSign")) {
-                ::X509_add_ext(x, ext, -1);
-                ::X509_EXTENSION_free(ext);
-            }
-        } else {
-            // Server cert (CN=localhost) needs SAN; client cert sets only EKU=clientAuth.
-            if (cn == "localhost") {
-                if (X509_EXTENSION* ext = ::X509V3_EXT_conf_nid(nullptr, &v3ctx,
-                        NID_subject_alt_name, "DNS:localhost,IP:127.0.0.1")) {
-                    ::X509_add_ext(x, ext, -1);
-                    ::X509_EXTENSION_free(ext);
-                }
-            } else {
-                if (X509_EXTENSION* ext = ::X509V3_EXT_conf_nid(nullptr, &v3ctx,
-                        NID_ext_key_usage, "clientAuth")) {
-                    ::X509_add_ext(x, ext, -1);
-                    ::X509_EXTENSION_free(ext);
-                }
-            }
-        }
-        ::X509_sign(x, signer_pkey, ::EVP_sha256());
-        return x;
-    }
-
-    static void write_pair(const stl::string& cert_path, const stl::string& key_path,
-                           X509* x509, EVP_PKEY* pkey) {
-        FILE* fp = std::fopen(cert_path.c_str(), "wb");
-        ::PEM_write_X509(fp, x509);
-        std::fclose(fp);
-        fp = std::fopen(key_path.c_str(), "wb");
-        ::PEM_write_PrivateKey(fp, pkey, nullptr, nullptr, 0, nullptr, nullptr);
-        std::fclose(fp);
-    }
-
-    fs::path  m_dir;
-    EVP_PKEY* m_pkey = nullptr;
-    X509*     m_x509 = nullptr;
-};
 
 // ---------------------------------------------------------------------------
 // Server / client helpers
